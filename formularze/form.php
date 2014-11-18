d193fbeb8e3684ad74f4f907002ddde3556f1484
zuzanowak <zuzanna_nowak@ekos.edu.pl>
2014-11-18 17:51:04 +0100

    Update form.php

--- formularze/form.php
+++ formularze/form.php
@@ -1,3 +1,8 @@
 <?php
+if(is_numeric($_GET['dzien']) && is_numeric($_GET['miesiac']) && is_numeric($_GET['rok']) && is_string($_GET['imie']) && is_string($_GET['nazwisko'])){
 echo $_GET['imie'].' '.$_GET['nazwisko'].' '.$_GET['dzien'].'-'.$_GET['miesiac'].'-'.$_GET['rok'];
+}
+else {
+  echo 'niepoprawne dane';
+}
 ?>
