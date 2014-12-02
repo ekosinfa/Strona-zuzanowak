8b65e1d8d0f0a5af2066b987f245dc85d455512b
zuzanowak <zuzanna_nowak@ekos.edu.pl>
2014-12-02 18:06:25 +0100

    Update mysqli.php

--- mysqli.php
+++ mysqli.php
@@ -5,7 +5,14 @@ $result = mysqli_query($con,$query);
 while($row = mysqli_fetch_array($result)){
   var_dump($row);
 }
-
+<table border="5">
+<tr>
+	<td>$row[id]</td>	<td>$row</td>
+</tr>
+<tr>
+	<td></td>	<td></td>
+</tr>
+</table>
 
 
 ?>
