081d0132758a6c4fcac2cddd1c0d5d3045b09433
zuzanowak <zuzanna_nowak@ekos.edu.pl>
2014-12-02 17:51:15 +0100

    Update mysqli.php

--- mysqli.php
+++ mysqli.php
@@ -1,6 +1,6 @@
 <?php
 $con=mysqli_connect("localhost","zuza","zuzaxsw21qaz","ekosinfa_zuza");
-$query="SELECT * FROM 'esport' ".$co."";
+$query="SELECT * FROM 'esport'";
 $result = mysqli_query($con,$query);
 while($row = mysqli_fetch_array($result));
 var_dump($row);
